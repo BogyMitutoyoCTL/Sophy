854ec416846df44a9e6502ea947a398d74c4215a
https://LukasBenner@github.com <lukasbenner02@gmail.com>
2018-03-19 11:11:03 +0000

    - Jetzt 2 Lichtschranken - Objektorientierung verbessert     Pin_Variable nun Objektgesteuert     Pin_Setup ebenfalls - Nur noch einmal "time"

--- source/LightBarrier.py
+++ source/LightBarrier.py
@@ -1,34 +1,33 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO_PIN = 4
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down= GPIO.PUD_DOWN)
+
 
 
 class LightBarrier:
-    def __init__(self):
-        self.first_time = 0
-        self.second_time = 0
+    def __init__(self, GPIO_PIN):
+        self.time = 0
+        self.GPIO_PIN = GPIO_PIN
+        GPIO.setup(self.GPIO_PIN, GPIO.IN, pull_up_down= GPIO.PUD_DOWN)
 
     def trigger(self):
-        GPIO.wait_for_edge(GPIO_PIN, GPIO.RISING)
-        self.first_time = time.time()
+        GPIO.wait_for_edge(self.GPIO_PIN, GPIO.RISING)
+        self.time = time.time()
 
 
-        GPIO.wait_for_edge(GPIO_PIN, GPIO.FALLING)
-
-        GPIO.wait_for_edge(GPIO_PIN, GPIO.RISING)
-        self.second_time = time.time()
+        GPIO.wait_for_edge(self.GPIO_PIN, GPIO.FALLING)
 
 
-LightBarrier1 = LightBarrier()
+
+LightBarrier1 = LightBarrier(4)
 LightBarrier1.trigger()
 
-LightBarrier2 = LightBarrier()
+
+LightBarrier2 = LightBarrier(17)
 LightBarrier2.trigger()
 
-
-print(signals.first_time)
-print(signals.second_time)
+print(LightBarrier1.time)
+print(LightBarrier2.time)
