48ace19bb51674d663cd76814d4b1db712f6077e
Vsevolod Pypenko <pypenko.vv1402s@gmail.com>
2018-03-19 11:29:21 +0100

    Update Files

--- source/TimeDifference.py
+++ source/TimeDifference.py
@@ -2,13 +2,15 @@
 """
 Calculate time difference from two times
 """
+from datetime import datetime
+from datetime import timedelta
 
 
 class TimeDifference:
     def __init__(self):
         pass
 
-    def calculate(self, time1, time2):
+    def calculate(self, time1: datetime, time2: datetime) -> timedelta:
         return time2 - time1
 
 
