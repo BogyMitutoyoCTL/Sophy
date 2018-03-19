66a087c58f4235811b6975ab4d44db9c435cf86d
Vsevolod Pypenko <pypenko.vv1402s@gmail.com>
2018-03-19 09:22:11 +0100

    Add TimeDifference.py

---
+++ source/TimeDifference.py
@@ -0,0 +1,10 @@
+import time
+
+class TimeDifference:
+    def __init__(self):
+        self.description = "Calculate time difference from two times"
+
+    def calculate(self, firstTime, secondTime):
+        datetime t1 = firstTime
+        datetime t2 = secondTime
+
