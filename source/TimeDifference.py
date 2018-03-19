ed115345c85334aea28a110037e24435d50ccef2
Vsevolod Pypenko <pypenko.vv1402s@gmail.com>
2018-03-19 11:00:42 +0100

    Update Files

--- source/TimeDifference.py
+++ source/TimeDifference.py
@@ -1,11 +1,18 @@
 
+"""
+Calculate time difference from two times
+"""
+
 
 class TimeDifference:
     def __init__(self):
-        self.description = "Calculate time difference from two times"
+        pass
 
     def calculate(self, time1, time2):
+        difftime = time2 - time1
         print(time1.strftime("%b %d %Y %H:%M:%S:%f"))
         print(time2.strftime("%b %d %Y %H:%M:%S:%f"))
+        # print(str(difftime.microseconds / 1000))
+        print(str(difftime))
 
 
--- source/testmain.py
+++ source/testmain.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 
+import unittest
 from datetime import datetime
 import time
 from TimeDifference import TimeDifference
 
-if __name__ == "__main__":
-    td = TimeDifference()
-    dt1 = datetime.now()
-    time.sleep(0.004)
-    dt2 = datetime.now()
-    td.calculate(dt1, dt2)
 
+class TestTimeDifference(unittest.TestCase):
+
+    def test_tbd(self):
+        td = TimeDifference()
+        dt1 = datetime.now()
+        time.sleep(5)
+        dt2 = datetime.now()
+        td.calculate(dt1, dt2)
+        self.assertEqual(True, True)
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
