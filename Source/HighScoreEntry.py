0fd58291c3ff820ec73bcec73fbb69a6983bba0a
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2018-03-19 15:10:04 +0100

    Data container for a high score entry

---
+++ Source/HighScoreEntry.py
@@ -0,0 +1,25 @@
+"""
+HighScoreEntry: Information required for an entry in the high score list.
+This is just a data container. This class cannot do something.
+"""
+from datetime import timedelta
+from datetime import date
+
+
+class HighScoreEntry:
+    def __init__(self, name: str, duration: timedelta, speed: float, record_date: date):
+        """
+        Initializes the data class with all available values.
+        :param name: Name of the object or person holding the record.
+        :param duration: Time needed to pass all light barriers.
+                         This value can be used for sorting the high score list.
+        :param speed: Speed in km/h, calculated from the duration and the distance of the light barriers.
+                      This value is probably better understood by humans compared to duration.
+        :param record_date: Date when the record was achieved.
+                            This value can be used for building a weekly, monthly and all-time
+                            high score list
+        """
+        self.name = name
+        self.duration = duration
+        self.speed = speed
+        self.record_date = record_date
