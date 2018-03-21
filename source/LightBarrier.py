1d83e654a9e712ce3b72cf38bf20a8c9cda92869
https://LukasBenner@github.com <lukasbenner02@gmail.com>
2018-03-21 08:43:03 +0000

    Ändern der Signalverarbeitung von wait_for_edge zu event_detect Nun kann man mehrere Signale wahrnehmen

--- source/LightBarrier.py
+++ source/LightBarrier.py
@@ -5,20 +5,22 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 
+
+
 class LightBarrier:
     def __init__(self, GPIO_PIN):
         self.time = 0
         self.GPIO_PIN = GPIO_PIN
-
         GPIO.setup(self.GPIO_PIN, GPIO.IN, pull_up_down= GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.GPIO_PIN, GPIO.RISING, callback= self.mycallback, bouncetime=200)
 
-
-    def trigger(self):
-        GPIO.wait_for_edge(self.GPIO_PIN, GPIO.RISING)
+    def mycallback(self,GPIO_PIN):
         self.time = time.time()
+        print(self.time)
 
 
-        GPIO.wait_for_edge(self.GPIO_PIN, GPIO.FALLING)
-        return self.time
+if __name__ == '__main__':
+    LightBarrier(4)
+    time.sleep(10)
 
 
