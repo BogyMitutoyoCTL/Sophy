1cf4cae841714667789d4a3191b3174eb5626f64
Vsevolod Pypenko <pypenko.vv1402s@gmail.com>
2018-03-21 14:27:18 +0100

    Made high score list. Improved HighScoreEntry and TestHighScoreEntry.

--- source/TestHighScoreEntry.py
+++ source/TestHighScoreEntry.py
@@ -11,7 +11,7 @@ class TestHighScoreEntry(unittest.TestCase):
         entry = HighScoreEntry("John", timedelta(seconds=3, milliseconds=450), 34.8, date(2018, 3, 2))
 
         self.assertEqual(entry.name, "John")
-        self.assertEqual(entry.duration, timedelta(0,3,0,450,0,0,0))
+        self.assertEqual(entry.duration, timedelta(0, 3, 0, 450, 0, 0, 0))
         self.assertEqual(entry.record_date, date(2018, 3, 2))
         self.assertEqual(entry.speed, 34.8)
 
